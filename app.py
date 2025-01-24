a21d6ac4d7f06a94f6755c8b2b9d9596ce60b0e6
CodeHemP <92779379+CodeHemP@users.noreply.github.com>
2025-01-24 12:41:22 +0530

    app.py

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ if st.button("Predict"):
     # Prepare the input data for the model
     input_data = np.array([[x1, x2]])
     prediction = model.predict(input_data)
+    st.success(f"The predicted prob is: {prediction}")
     # Convert prediction to binary output (0 or 1)
     output = 1 if prediction[0][0] > 0.5 else 0
     # Display the result
